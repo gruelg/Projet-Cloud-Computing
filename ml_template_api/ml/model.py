c632bce238cd4ed6b081f61522d6455877b8e61c
gruelg <gruelg@outlook.com>
2021-01-27 20:53:56 +0100

    debut api

--- ml_template_api/ml/model.py
+++ ml_template_api/ml/model.py
@@ -1,1 +1,20 @@
-import utils
+from utils.utils import *
+
+class model:
+
+    def __inti__(self):
+        pass
+
+    def creatModel(self):
+        data = DataHandler()
+        data.get_data()
+        featuresRecipe = FeatureRecipe(data.df_vgsales)
+        featuresRecipe.prepareData(3)
+        extractor = FeatureExtractor(featuresRecipe.df)
+        extractor.train()
+        buildModel = ModelBuilder("", True)
+        buildModel.calculData(extractor.X, extractor.y)
+
+    def loadModel(self):
+        pass
+
